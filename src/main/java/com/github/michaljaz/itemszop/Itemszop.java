6a7a3772456c1a702071512cde3997a36a52c2a7
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-04-05 17:15:14 +0200

    create blank project

---
+++ src/main/java/com/github/michaljaz/itemszop/Itemszop.java
@@ -0,0 +1,17 @@
+package com.github.michaljaz.itemszop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Itemszop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
