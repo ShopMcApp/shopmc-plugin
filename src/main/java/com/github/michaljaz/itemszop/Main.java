c6224aaeb199d47d9335fc2c6007fa61b8b64878
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-04-17 12:33:45 +0200

    simple http request

--- src/main/java/com/github/michaljaz/itemszop/Main.java
+++ src/main/java/com/github/michaljaz/itemszop/Main.java
@@ -1,7 +1,13 @@
 package com.github.michaljaz.itemszop;
 
+import eu.dattri.jsonbodyhandler.JsonBodyHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Main extends JavaPlugin {
 
@@ -16,6 +22,14 @@ public class Main extends JavaPlugin {
                 "§6|_| \\__| \\___||_| |_| |_||___//___| \\___/ | .__/ \n §2" +
                 "                          " + this.getDescription().getVersion() + "            §6| |    \n" +
                 "\n" + "§fDeveloped by " + this.getDescription().getAuthors() + " dla https://github.com/michaljaz/itemszop §a" + "\n§fPlugin został załadowany w §a" + (System.currentTimeMillis() - startTime) + "ms§7.\n§fWykryty silnik: " + Bukkit.getVersion().split("-")[1]);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/"))
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
     }
 
     @Override
