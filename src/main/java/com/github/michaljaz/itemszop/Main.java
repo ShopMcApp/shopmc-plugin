3bb04edb0e608ad96b030749eaae384ab95f7eb6
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-04-17 08:16:21 +0200

    authors & chat color fix

--- src/main/java/com/github/michaljaz/itemszop/Main.java
+++ src/main/java/com/github/michaljaz/itemszop/Main.java
@@ -1,6 +1,5 @@
 package com.github.michaljaz.itemszop;
 
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,16 +8,14 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         long startTime = System.currentTimeMillis();
-        getLogger().info("\n" +
-                "§6 _  _                                            \n" +
+        Bukkit.getConsoleSender().sendMessage("\n" +
                 "§6(_)| |                                           \n" +
                 "§6 _ | |_   ___  _ __ ___   ___  ____  ___   _ __  \n" +
                 "§6| || __| / _ \\| '_ ` _ \\ / __||_  / / _ \\ | '_ \\ \n" +
                 "§6| || |_ |  __/| | | | | |\\__ \\ / / | (_) || |_) |\n" +
                 "§6|_| \\__| \\___||_| |_| |_||___//___| \\___/ | .__/ \n §2" +
-                "                          " + this.getDescription().getVersion() + "   §6| |    \n" +
+                "                          " + this.getDescription().getVersion() + "            §6| |    \n" +
                 "\n" + "§fDeveloped by " + this.getDescription().getAuthors() + " dla https://github.com/michaljaz/itemszop §a" + "\n§fPlugin został załadowany w §a" + (System.currentTimeMillis() - startTime) + "ms§7.\n§fWykryty silnik: " + Bukkit.getVersion().split("-")[1]);
-
     }
 
     @Override
