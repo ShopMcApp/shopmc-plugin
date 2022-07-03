e06aadc2485a68213047451c80cb8275101e948a
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-07-03 10:21:35 +0200

    decode config key

--- src/main/java/com/github/michaljaz/itemszop/Main.java
+++ src/main/java/com/github/michaljaz/itemszop/Main.java
@@ -10,18 +10,29 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Base64;
 
 public class Main extends JavaPlugin {
+    String key;
+    String firebaseWebsocketUrl;
     String serverId;
-    String databaseUrl;
-    String dbID = "sklepmc-c7516-default-rtdb";
-    String firebaseWebsocketUrl = "s-euw1b-nss-207.europe-west1.firebasedatabase.app";
+    String secret;
 
     void connectToFirebase() throws IOException, WebSocketException {
         System.out.println("Connecting to " + firebaseWebsocketUrl + "...");
-        WebSocket ws = new WebSocketFactory().createSocket("wss://" + firebaseWebsocketUrl + "/.ws?v=5&ns=" + dbID);
+        WebSocket ws = new WebSocketFactory().createSocket(firebaseWebsocketUrl);
 
         ws.addListener(new WebSocketAdapter() {
+            @Override
+            public void onConnected(WebSocket websocket, Map<String, List<String>> headers) throws Exception {
+                super.onConnected(websocket, headers);
+                System.out.println("connected!");
+            }
+
             @Override
             public void onTextMessage(WebSocket websocket, String message) throws ParseException, IOException, WebSocketException {
                 // Received a text message.
@@ -30,34 +41,35 @@ public class Main extends JavaPlugin {
                 JSONObject json = (JSONObject) parser.parse(message);
                 JSONObject json_data = (JSONObject) parser.parse(json.get("d").toString());
                 String data = json_data.get("d").toString();
-                boolean requiresUpdate = json_data.get("t").equals("r");
-                if(requiresUpdate){
-                    ws.disconnect();
-                    firebaseWebsocketUrl = data;
-                    connectToFirebase();
-                }
             }
         });
         ws.connect();
+        ws.sendText("{\"t\":\"d\",\"d\":{\"r\":2,\"a\":\"q\",\"b\":{\"p\":\"/servers/gitcraft/commands\",\"h\":\"\"}}}");
     }
 
     @Override
     public void onEnable() {
-        try {
-            connectToFirebase();
-        } catch (IOException | WebSocketException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            connectToFirebase();
+//        } catch (IOException | WebSocketException e) {
+//            e.printStackTrace();
+//        }
         // config file
         FileConfiguration config = this.getConfig();
-        config.addDefault("serverId", "");
-        config.addDefault("databaseUrl", "https://sklepmc-c7516-default-rtdb.europe-west1.firebasedatabase.app"); // default database
-        config.addDefault("triggerPort", 8001);
+        config.addDefault("key", "");
         config.options().copyDefaults(true);
         saveConfig();
-        serverId = config.getString("serverId");
-        databaseUrl = config.getString("databaseUrl");
+        key = config.getString("key");
 
+        byte[] decoded = Base64.getDecoder().decode(key);
+        String decodedStr = new String(decoded, StandardCharsets.UTF_8);
+        String[] stringList = decodedStr.split("@");
+        secret = stringList[0];
+        firebaseWebsocketUrl = stringList[1];
+        serverId = stringList[2];
+        System.out.println(secret);
+        System.out.println(firebaseWebsocketUrl);
+        System.out.println(serverId);
         // intro
         String intro = "\n" +
                 ChatColor.BLUE + "(_)| |                                           \n" +
@@ -65,7 +77,7 @@ public class Main extends JavaPlugin {
                 ChatColor.BLUE + "| || __| / _ \\| '_ ` _ \\ / __||_  / / _ \\ | '_ \\ \n" +
                 ChatColor.BLUE + "| || |_ |  __/| | | | | |\\__ \\ / / | (_) || |_) |\n" +
                 ChatColor.BLUE + "|_| \\__| \\___||_| |_| |_||___//___| \\___/ | .__/ \n" +
-                "                          " + ChatColor.AQUA + this.getDescription().getVersion() + "            " + ChatColor.BLUE + "| |    \n" +
+                "                          " + ChatColor.AQUA + this.getDescription().getVersion() + "            " + ChatColor.BLUE + " | |    \n" +
                 "\n" + ChatColor.DARK_BLUE + "Plugin do sklepu serwera - https://github.com/michaljaz/itemszop-plugin \n" +
                 ChatColor.DARK_BLUE + "Autorzy: " + this.getDescription().getAuthors() + "\n" +
                 ChatColor.DARK_BLUE + "Id serwera: " + ChatColor.AQUA + serverId + "\n";
