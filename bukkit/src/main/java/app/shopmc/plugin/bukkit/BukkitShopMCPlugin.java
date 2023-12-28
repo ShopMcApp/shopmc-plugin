2d8138b2b415ee72d689a439b5aff1148315c580
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2023-12-28 16:37:38 +0100

    print simple text

--- bukkit/src/main/java/app/shopmc/plugin/bukkit/BukkitShopMCPlugin.java
+++ bukkit/src/main/java/app/shopmc/plugin/bukkit/BukkitShopMCPlugin.java
@@ -1,12 +1,12 @@
 package app.shopmc.plugin.bukkit;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-@SuppressWarnings("ConstantConditions")
 public class BukkitShopMCPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        Bukkit.getConsoleSender().sendMessage("WORKING!");
     }
 
     @Override
